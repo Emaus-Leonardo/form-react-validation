d0b29829b946e1b572025483b22419b9e4c265fe
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2024-05-09 23:38:42 -0300

    adicionando toastfy e submit no form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import FormInput from "./components/FormInput.jsx";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     confirmPassword: "",
   });
 
+  const formRef = useRef(null); // Referência para o formulário
+
   const input = [
     {
       id: 1,
@@ -17,20 +21,23 @@ function App() {
       type: "text",
       placeholder: "Username",
       label: "Username",
+      required: true,
     },
     {
       id: 2,
       name: "email",
-      type: "text",
+      type: "email",
       placeholder: "Email",
       label: "Email",
+      required: true,
     },
     {
       id: 3,
       name: "birthday",
-      type: "text",
+      type: "date",
       placeholder: "Birthday",
       label: "Birthday",
+      required: true,
     },
     {
       id: 4,
@@ -38,6 +45,7 @@ function App() {
       type: "password",
       placeholder: "Password",
       label: "Password",
+      required: true,
     },
     {
       id: 5,
@@ -45,6 +53,7 @@ function App() {
       type: "password",
       placeholder: "Confirm Password",
       label: "Confirm Password",
+      required: true,
     },
   ];
 
@@ -52,8 +61,21 @@ function App() {
     e.preventDefault();
     const data = new FormData(e.target);
     console.log(Object.fromEntries(data.entries()));
-  };
+  
+    // Resetando o formulário
+    formRef.current.reset();
 
+    setValues({
+      username: "",
+      email: "",
+      birthday: "",
+      password: "",
+      confirmPassword: "",
+    });
+  
+    toast.success("Formulário enviado com sucesso!");
+  };
+  
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
@@ -61,10 +83,19 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen">
       <form
+        ref={formRef}
         onSubmit={handleSubmit}
-        className="flex flex-col items-center bg-white rounded-xl p-5 shadow-xl "
+        className="flex flex-col bg-white rounded-xl p-5 shadow-xl"
       >
-        <div className="">
+        <div className="flex  gap-1 ">
+          <div className="rounded-full w-2 h-2 bg-green-500"></div>
+          <div className="rounded-full w-2 h-2 bg-red-500"></div>
+          <div className="rounded-full w-2 h-2 bg-yellow-400"></div>
+        </div>
+        <h1 className="font-bold flex justify-center text-[22px]">
+          Form Register
+        </h1>
+        <div>
           {input.map((inputField) => (
             <FormInput
               key={inputField.id}
@@ -75,10 +106,11 @@ function App() {
             />
           ))}
         </div>
-        <button className="bg-blue-500 hover:bg-blue-700 transition-all text-white font-bold py-2 px-4 rounded mt-4">
+        <button className="bg-blue-500 hover:bg-blue-700 transition-all flex justify-center items-center w-full text-white font-bold py-2 px-4 rounded mt-4 self-center">
           Submit
         </button>
       </form>
+      <ToastContainer />
     </div>
   );
 }
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,17 +1,18 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function FormInput({ label, onChange, id, ...inputProps }) {
   const inputId = String(id);
 
   return (
-    <div className="p-5 m-[5px]">
+    <div className="mb-7 w-full">
       <label htmlFor={inputId}>{label}</label>
       <input
         id={inputId}
-        className="h-8 w-full outline-none bg-fundo rounded-lg p-2 shadow-2xl"
+        className="h-8 w-full outline-none bg-fundo rounded p-2 shadow-2xl"
         {...inputProps}
         onChange={onChange}
       />
+
     </div>
   );
 }
@@ -19,7 +20,7 @@ function FormInput({ label, onChange, id, ...inputProps }) {
 FormInput.propTypes = {
   label: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired, 
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
 
 export default FormInput;
